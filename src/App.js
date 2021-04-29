6f36ee34a46a96ef5c81b00d209e107eab10aa76
AMRIBOUTHEINA <bouthein_amri@yahoo.com>
2021-04-30 01:00:03 +0200

    chekpointjsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import myWonderfulImage from "./jsxsource.png";
+import myWonderfulVideo from "./tuto.mp4";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div style={{borderStyle:'solid' ,borderSize:1 ,borderColor:'black' ,maxWidth:'100vw'}}>
+
+      <h1 className="title red">AMRI BOUTHEINA</h1>
+
+      <br/>
+ 
+ <img style={{width:350 ,height:250 }}  src={myWonderfulImage} alt ='jsxsource' />
+
+
+ <br/>
+<img style={{width:350 ,height:250 }} src='/jsxpublic.png' alt='jsxpublic' />
+</div>
+<video style={{height:240 ,width:320}} controls autostart autoPlay src={myWonderfulVideo} type="video/mp4"  />
+
+
       </header>
     </div>
   );
